f9147019e0b6e3bc502681503cf6823fca6ba9a6
BuiDuongDucTri <140304627+BuiDuongDucTri@users.noreply.github.com>
2023-12-24 15:52:41 +0700

    Nộp bài checkpoint1

---
+++ checkpoint1/index.js
@@ -0,0 +1,10 @@
+// 9.
+// const m =[1,2,3,4,5,6,"hh", "9",80,100];
+// const filteredArray = m.filter(item => typeof item === 'number');
+// const mSquared = filteredArray.map(item =>Math.pow(item,2))
+// console.log(mSquared)
+// 10.var inputString = "High knowledge, high return";
+// var charArray = inputString.split('');
+// console.log("mang sau khi chuyen doi:",charArray);
+// var wordArray = inputString.split('');
+// console.log("mang tu chuoi:",wordArray);
